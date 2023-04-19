9fc2ca6606b1c3d9be11be216e993fbe7f4d6a28
ans4asif <ans4asif@gitlab.com>
2023-04-20 02:05:29 +0500

    add Fields Component

--- thaw-ui/src/app/pitch-deck/page.tsx
+++ thaw-ui/src/app/pitch-deck/page.tsx
@@ -1,23 +1,31 @@
-import React from 'react';
+'use client'
+import React  from 'react';
 import Button from '../../../components/Button';
 import PageTitle from '../../../components/PageTitle';
 import Image from 'next/image';
 import Deck from '../../../assets/pitch_deck.png'
+import Field from '../../../components/Field';
 const PitchDeck = () => {
   return (
     <div className='container'>
       <div className='pt-8 pb-4 flex justify-between'>
         <PageTitle title='Pitch Deck' subTitle='Manage your Pitch Decks, or create a new Pitch Deck' />
-       
       </div>
+      <Field
+          type='toggle'
+          label='Deck Name'
+          value=''
+          name="deck_name"
+          onChange={()=>{}}
+          />
       <section className='flex justify-center content-center min-h-screen'>
         <div className='my-auto mx-0'>
           <div className="flex justify-center">
           <Image src={Deck} alt="deck"/>
           </div>
           <div className='text-center py-7'>
-            <h2 className='font-general-sans-regular text-2xl text-primary-text'>No Pitch Deck Yet!</h2>
-            <p className="font-general-sans-regular text-sm text-secondery-text">Create Your Pitch Deck Now</p>
+          <h4 className="font-medium text-[20px]">No Pitch Deck Yet!</h4>
+        <p className="text-[14px] text-[#3A383E]">Create Your Pitch Deck Now</p>
           </div>
           <div className="mt-2">
           <Button type='primary' icon='plus'>
@@ -26,6 +34,7 @@ const PitchDeck = () => {
           </div>
         </div>
       </section>
+
     </div>
   );
 };
